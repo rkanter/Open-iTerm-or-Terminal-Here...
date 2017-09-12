8bdfdb1e1b6465f89e9932cd9d769a5326411a85
Robert Kanter <rkanter@cloudera.com>
2017-09-11 19:46:12 -0700

    initial version of iTerm setup script

---
+++ make_iTerm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# TODO:
+# Parameterize this with default values
+# Make common script for Terminal version
+# printouts
+# what if the file already exists?
+# error handling
+# clean up, etc
+
+
+# Bundle the iTerm script into an Application
+osacompile -o /Applications/Open\ iTerm\ Here....app applescripts/iTerm.scpt
+
+# Copy over the iTerm icon
+cp -f /Applications/iTerm.app/Contents/Resources/AppIcon.icns /Applications/Open\ iTerm\ Here....app/Contents/Resources/applet.icns
+
+# Set internal app property to make it not show up in the Dock
+# Otherwise, it looks weird because it opens and then closes <1 second later
+plutil -replace LSUIElement -bool YES  /Applications/Open\ iTerm\ Here....app/Contents/Info.plist
